808ebcdcc1c050ffff089746d8e157b6e4a49bec
Stanislas Bruhière <stanislas@bruhiere.fr>
2024-03-21 18:35:46 +0100

    feat: start working with ws

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import './App.css';
 import Setup from './pages/Setup'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function App() {
+  const connection = React.useRef<WebSocket | null>(null)
+  useEffect(() => {
+    const ws = new WebSocket('ws://localhost:8000/ws')
+    ws.onopen = (event) => {
+      console.log('connected')
+      ws.send('hello')
+    }
+    ws.onmessage = (event) => {
+      console.log('message', event.data)
+    }
+    connection.current = ws
+    return () => ws.close()
+  })
   return (
     <div>
       <Setup/>
